6a8ee23ae6b9bb38facb370e1733ab84031399dc
AndreWolf <sabou.andrei26@gmail.com>
2023-11-06 00:38:15 +0200

    Lesson 8

---
+++ lesson 8/scripts/DelegateVote.ts
@@ -0,0 +1,47 @@
+import { ethers } from "ethers";
+import { Ballot, Ballot__factory } from "../typechain-types";
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS || '';
+
+async function main() {
+    //receive parameters
+    const parameters = process.argv.slice(2);
+    if (!parameters || parameters.length < 1)
+        throw new Error("Parameters not provided");
+    // const contractAddress = parameters[0];
+    const iugaDelegate = '0x1E03b84cC47C523c2A4b1EFf5A42a86dC673E9C5';
+    console.log('iugaDelegate', iugaDelegate);
+    //configure provider
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+    const lastBlock = await provider.getBlock('latest');
+    console.log(`Last block number: ${lastBlock?.number}`);
+    const lastBlockTimestamp = lastBlock?.timestamp ?? 0;
+    const lastBlockDate = new Date(lastBlockTimestamp * 1000);
+    console.log(`Last block timestamp: ${lastBlockTimestamp} (${lastBlockDate.toLocaleDateString()} ${lastBlockDate.toLocaleTimeString()})`);
+
+    //configure wallet
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+    console.log(`Using address ${wallet.address}`);
+    const balanceBN = await provider.getBalance(wallet.address);
+    const balance = Number(ethers.formatUnits(balanceBN));
+    console.log(`Wallet balance ${balance} ETH`);
+    if (balance < 0.01) {
+        throw new Error("Not enough ether");
+    }
+
+    //attach contract
+    const ballotFactory = new Ballot__factory(wallet);
+    const ballotContract = ballotFactory.attach(CONTRACT_ADDRESS) as Ballot;
+
+    const tx = await ballotContract.delegate(iugaDelegate)
+    const receipt = await tx.wait();
+    console.log(`Transaction completed ${receipt?.hash}`)
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
---
+++ lesson 8/scripts/GiveRightToVote.ts
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+import { Ballot, Ballot__factory } from "../typechain-types";
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS ?? "";
+
+async function main() {
+    const parameters = process.argv.slice(2);
+    if (!parameters || parameters.length < 0)
+        throw new Error("Parameters not provided");
+    const address = parameters[0];
+
+    //configure provider
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+
+    //configure wallet
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
+    console.log(`Using address ${wallet.address}`);
+    const ballotFactory = new Ballot__factory(wallet);
+    const ballotContract = ballotFactory.attach(CONTRACT_ADDRESS) as Ballot;
+    const tx = await ballotContract.giveRightToVote(address);
+    const receipt = await tx.wait();
+    console.log(`Transaction completed ${receipt?.hash}`)
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
